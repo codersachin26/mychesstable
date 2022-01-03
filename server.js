61c44f5422cfeb07183eb4a9db516da1a2f3f66d
Sachin Indoriya <57168948+codersachin26@users.noreply.github.com>
2022-01-03 22:24:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.get("/:code",async(req,res) =>{
 
 });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+const PORT = process.env.PORT ||5000;
+app.listen(PORT, () => {
+ console.log("Server running on port 5000");
 });
